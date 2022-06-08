ec65720bf80c0c37d65adc55bd8c506efc45ee63
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-08 23:04:28 +0300

    flags added

--- src/index.js
+++ src/index.js
@@ -5,25 +5,53 @@ const DEBOUNCE_DELAY = 300;
 const SEARCH_NAME_URL = `https://restcountries.com/v3.1/name/`;
 
 const refs = {
-    searchBox: document.querySelector(`#search-box`)
+    searchBox: document.querySelector(`#search-box`),
+    countryList: document.querySelector(`.country-list`),
 }
 
-refs.searchBox.addEventListener(`input`, debounce(onSearchBoxSubmit, 300))
+const options = {
+    headers: {
+        fullText: true
+    }
+}
+
+refs.searchBox.addEventListener(`input`, debounce(onSearchBoxSubmit, DEBOUNCE_DELAY))
 
 function onSearchBoxSubmit(event) {
     event.preventDefault();
-    const name = event.target.value;
+    const name = event.target.value.trim();
+    console.log(name)
 
     fetchArticles(name);
 }
 
 function fetchArticles(name) {
-    const url = `${SEARCH_NAME_URL}${name}`
+    const url = `${SEARCH_NAME_URL}${name}?fields=name,capital,population,flags,languages`
     console.log(url )
 
-    return fetch(url).then(response => response.json())
-        .then((country) => {
-            country[0].name.official
-            country.map(state => console.log(state.name.official))
+    return fetch(url, options).then(response => response.json())
+        .then(countryMarkup)
+}
+
+function countryMarkup(countries) {
+    console.log(countries)
+
+    if (countries.length === 1) {
+        countries.map(country => {
+            const nameEl = country.name.official;
+            const flag = country.flags.svg;
+            const language = Object.values(country.languages).join(`, `);
+            console.log(language)
+            const capital = country.capital;
+            const population = country.population;
+
+            refs.countryList.insertAdjacentHTML(`beforeend`, 
+            `<li>
+            <h2 class="country__name"><span class="country__flag"><img src="${flag}" alt="country flag" height = "40"/></span>${nameEl}</h2>
+            <p><b>Capital:</b> ${capital}</p>
+            <p><b>Population:</b> ${population}</p>
+            <p><b>Language:</b> ${language}</p>
+           </li>`)
         })
+    }
 }
