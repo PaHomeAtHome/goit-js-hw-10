c20f9e365add0e530374797e0c43f8f76d0af1b4
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-08 19:51:13 +0300

    debounce added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import debounce from "lodash.debounce"
 
 const DEBOUNCE_DELAY = 300;
+const SEARCH_NAME_URL = `https://restcountries.com/v3.1/name/`;
+
+const refs = {
+    searchBox: document.querySelector(`#search-box`)
+}
+
+refs.searchBox.addEventListener(`input`, debounce(onSearchBoxSubmit, 300))
+
+function onSearchBoxSubmit(event) {
+    event.preventDefault();
+    const name = event.target.value;
+
+    fetchArticles(name);
+}
+
+function fetchArticles(name) {
+    const url = `${SEARCH_NAME_URL}${name}`
+    console.log(url )
+
+    return fetch(url).then(response => response.json())
+        .then((country) => {
+            country[0].name.official
+            country.map(state => console.log(state.name.official))
+        })
+}
